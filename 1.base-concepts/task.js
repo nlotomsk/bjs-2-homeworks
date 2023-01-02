b0acfd64fe877f33098b7c91cb68b58c03a96919
Vyacheslav <93542374+nlotomsk@users.noreply.github.com>
2023-01-02 15:02:58 +0700

    performed homeworks 1 clear

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -31,34 +31,3 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
     return Number(totalSum.toFixed(2));
   }
 }
-
-//
-// const solveEquation = (a, b, c) => {
-//   const discriminant = ((b ** 2) - 4 * a * c);
-//   if (discriminant < 0) {
-//     return array = [];
-//   } else if (discriminant === 0) {
-//       return array = [-b / (2 * a)];
-//   } else {
-//       return [(-b + Math.sqrt(discriminant))/(2*a), (-b - Math.sqrt(discriminant)) / (2 * a)];
-//   }
-// };
-//
-// // const testCase = (a, b, c) => console.log(solveEquation(a, b, c))
-// // testCase(11, 122, 4);
-//
-// const calculateTotalMortgage = (percent, contribution, amount, countMonths) => {
-//   if (isNaN(percent) === true ||
-//       isNaN(contribution) === true ||
-//       isNaN(amount) === true ||
-//       isNaN(countMonths) === true) {
-//     return false
-//   } else {
-//     percent = (percent / 100) / 12;
-//     const loanBody = amount - contribution;
-//     const monthlyInstallment = loanBody * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
-//     const totalAmount = (monthlyInstallment * countMonths);
-//     return Number(totalAmount.toFixed(2));
-//   }
-// };
-//
