c3c5457c0db9735d31a001afe3e74fcae63a40cf
Vyacheslav <93542374+nlotomsk@users.noreply.github.com>
2023-01-03 20:19:44 +0700

    performed homeworks 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,32 @@
-function compareArrays(arr1, arr2) {
-  
-}
+// function compareArrays(arr1, arr2) {
+//   if (arr1.length === arr2.length) {
+//     for (let i = 0; i < arr1.length; i++) {
+//       if (arr1[i] === arr2[i]) {
+//         return true;
+//       } else {
+//         return false;
+//       }
+//     }
+//   } else {
+//     return false;
+//   }
+// }
+
+const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((n,i) => n === arr2[i]);
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+let avg = 0;
+if (users.length !== 0) {
+  users.filter(user => user.gender === gender).reduce((acc, item, index, arr)=> {
+    acc+=item.age;
+    if (index === arr.length - 1){
+        avg = acc / arr.length;
+          return;
+          }
+          return acc;
+        },0);
+    } else {
+    return 0;
+    }
+  return avg;
 }
